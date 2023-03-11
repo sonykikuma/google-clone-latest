69e3c77c39b1935d36aecb71038ab1e1aacb0f71
abha <abha@example.com>
2023-03-11 20:55:31 +0530

    fetch data and added error file

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="relative min-h-screen">
         {children}
         {/*footer*/}
         <Footer />
--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,7 +1,37 @@
-import React from "react";
+import WebSearchResults from "@/app/components/WebSearchResults";
+import Link from "next/link";
 
-const WebSearch = () => {
-  return <div>webeSearch</div>;
-};
+export default async function WebSearch({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
 
-export default WebSearch;
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("something went wrong");
+  }
+
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
+  if (!results) {
+    return (
+      <div
+        className="flex flex-col justify-center items-center pt-10
+    "
+      >
+        <h1 className="text-3xl mb-4">No result found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to homepage
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <>{results && <WebSearchResults results={data} />}</>;
+}
+//results.map((result) => <h1>{result.title}</h1>)
