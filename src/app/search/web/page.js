a3dbd1a3d610f0a7787f5701dbed5fe886f1d1b4
abha <abha@example.com>
2023-03-12 21:04:02 +0530

    removed force-dynamic

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+//export const dynamic = "force-dynamic";
 
 import WebSearchResults from "@/app/components/WebSearchResults";
 import Link from "next/link";
