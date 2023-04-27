5cdb4ac464e42604b29a0925e2141dd4a2928518
abha <abha@example.com>
2023-04-27 15:31:33 +0530

    fix the vercel problem

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,4 +1,4 @@
-//export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 import ImageSearchResults from "@/app/components/ImageSearchResults";
 import Link from "next/link";
